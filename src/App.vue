df0b4572c97029e53860f5bc6a380498ae2cd785
Hamarko <hamarkoav@gmail.com>
2021-01-27 14:08:21 +0200

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,264 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="campaign-title">
+            Pets
+    </h1>
+    <section class="campaigns">
+            <div class="campaign-wrapper">
+                <span class="button active" data-click="4b20aa37-9e1a-4155-82e0-386d171cb1f0">
+                    PETS
+                </span>
+            </div>
+
+            <div class="campaign-wrapper">
+                <span class="button" data-click="b0e78282-d457-4790-97f0-d5c0642bee4d">
+                    FOOD
+                </span>
+            </div>
+
+            <div class="campaign-wrapper">
+                <span class="button" data-click="10a827d5-13b5-4cd6-a2d0-9f22dd7738d1">
+                    PLANTS
+                </span>
+            </div>           
+    </section>
+    <main class="page-wrapper">      
+            <!-- AD's item open -->
+            <section>
+                <div class="card-block ">
+                    <div class="card__image">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/>
+                    </div>
+
+                    <div class="card__info">
+                        <h3>
+                            Doggo Dogg
+                        </h3>
+
+                        <input type="checkbox" name="trigger" id="trigger1">
+                        <label class="trigger-arrow" for="trigger1">
+                            <span></span>
+                        </label>
+
+                        <div class="card__info__extra">
+                            <p>
+                                A very good boi that loves playing fetch and ice-cream! Gentle with everyone. Scared of the rain.
+                            </p>
+
+                            <div class="card__action">
+                                <a class="button" href="#" style="display: inline">
+                                    Go for it!
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- AD's item close -->
+
+            <!-- AD's item open -->
+            <section>
+                <div class="card-block ">
+                    <div class="card__image">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/>
+                    </div>
+
+                    <div class="card__info">
+                        <h3>
+                            Doggo Dogg
+                        </h3>
+
+                        <input type="checkbox" name="trigger">
+                        <label class="trigger-arrow" for="trigger">
+                            <span></span>
+                        </label>
+
+                        <div class="card__info__extra">
+                            <p>
+                                A very good boi that loves playing fetch and ice-cream! Gentle with everyone. Scared of the rain.
+                            </p>
+
+                            <div class="card__action">
+                                <a class="button" href="#" style="display: inline">
+                                    Go for it!
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- AD's item close -->
+
+            <!-- AD's item open -->
+            <section>
+                <div class="card-block ">
+                    <div class="card__image">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/>
+                    </div>
+
+                    <div class="card__info">
+                        <h3>
+                            Doggo Dogg
+                        </h3>
+
+                        <input type="checkbox" name="trigger">
+                        <label class="trigger-arrow" for="trigger">
+                            <span></span>
+                        </label>
+
+                        <div class="card__info__extra">
+                            <p>
+                                A very good boi that loves playing fetch and ice-cream! Gentle with everyone. Scared of the rain.
+                            </p>
+
+                            <div class="card__action">
+                                <a class="button" href="#" style="display: inline">
+                                    Go for it!
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- AD's item close -->
+
+            <!-- AD's item open -->
+            <section>
+                <div class="card-block ">
+                    <div class="card__image">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/>
+                    </div>
+
+                    <div class="card__info">
+                        <h3>
+                            Doggo Dogg
+                        </h3>
+
+                        <input type="checkbox" name="trigger">
+                        <label class="trigger-arrow" for="trigger">
+                            <span></span>
+                        </label>
+
+                        <div class="card__info__extra">
+                            <p>
+                                A very good boi that loves playing fetch and ice-cream! Gentle with everyone. Scared of the rain.
+                            </p>
+
+                            <div class="card__action">
+                                <a class="button" href="#" style="display: inline">
+                                    Go for it!
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- AD's item close -->
+
+            <!-- AD's item open -->
+            <section>
+                <div class="card-block ">
+                    <div class="card__image">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/>
+                    </div>
+
+                    <div class="card__info">
+                        <h3>
+                            Doggo Dogg
+                        </h3>
+
+                        <input type="checkbox" name="trigger">
+                        <label class="trigger-arrow" for="trigger">
+                            <span></span>
+                        </label>
+
+                        <div class="card__info__extra">
+                            <p>
+                                A very good boi that loves playing fetch and ice-cream! Gentle with everyone. Scared of the rain.
+                            </p>
+
+                            <div class="card__action">
+                                <a class="button" href="#">
+                                    Go for it!
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- AD's item close -->
+
+
+            <!-- AD's item open -->
+            <section>
+                <div class="card-block ">
+                    <div class="card__image">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/>
+                    </div>
+
+                    <div class="card__info">
+                        <h3>
+                            Doggo Dogg
+                        </h3>
+
+                        <input type="checkbox" name="trigger">
+                        <label class="trigger-arrow" for="trigger">
+                            <span></span>
+                        </label>
+
+                        <div class="card__info__extra">
+                            <p>
+                                A very good boi that loves playing fetch and ice-cream! Gentle with everyone. Scared of the rain.
+                            </p>
+
+                            <div class="card__action">
+                                <a class="button" href="#" style="display: inline">
+                                    Go for it!
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>            <!-- AD's item close -->
+
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pets from './components/Pets.vue'
+
+document.addEventListener('click',  (event) => {
+                event.stopPropagation();
+
+                let label = event.target.matches("label") && event.target;
+                if (label) {
+                    label.parentElement.querySelector(".checkbox").click();
+                }
+            })
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pets
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  main > * {       
+      min-width: 53%;
+    }
+    .card-block {
+        position: relative;
+        overflow: visible;
+    }   
+    .campaigns { position: relative }    
+    
 </style>
